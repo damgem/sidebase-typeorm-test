3e115a8961f021f602f6d913812904da810fb3aa
Damian Gleumes <damgem@zoho.com>
2022-11-08 17:30:56 +0100

    change app.vue

--- app.vue
+++ app.vue
@@ -1,8 +1,38 @@
+<script setup lang="ts">
+// import { AppDataSource } from './server/database'
+import { Example } from '~/server/database/entities/Example'
+
+const insertExample = async () => {
+  const exampleData = {
+    description: 'blah',
+    details: 'blub',
+  }
+
+  // Will throw `DataSource is not set for this entity.` error when executed
+  const example = new Example()
+  await example.update(exampleData) // internally calls `.save()`
+
+  // // Will throw `DataSource is not set for this entity.` error when executed
+  // await Example.save<Example>(exampleData)
+
+  // // Will throw `connection.driver.options is undefined` error on page load
+  // await AppDataSource.manager.save(Example, exampleData)
+
+  // // Will throw `connection.driver.options is undefined` error on page load
+  // const exampleRepository = AppDataSource.getRepository(Example)
+  // await exampleRepository.save(exampleData)
+}
+</script>
+
 <template>
   <div class="h-screen w-full bg-white dark:bg-gray-800 dark:text-gray-100">
     <Head>
       <Title>sidebase</Title>
     </Head>
-    <NuxtPage />
+    <div class="grid place-content-center h-screen w-screen">
+      <a-button @click="insertExample">
+        Insert Example Entity
+      </a-button>
+    </div>
   </div>
 </template>
